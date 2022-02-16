d6e4423654795739753c3dcb689704bfdffeab09
Ivan Hrabcak <ivan@hrabcak.eu>
2022-02-16 21:13:28 +0100

    Add forgotten file

---
+++ src/edupage/trait_implementations/ringing.rs
@@ -0,0 +1,9 @@
+use crate::edupage::{edupage_traits::Ringing, edupage::Edupage};
+
+
+
+impl Ringing for Edupage {
+    fn get_ringing_times() -> Vec<crate::edupage::edupage_traits::LessonTime> {
+        todo!()
+    }
+}
